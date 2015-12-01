b42be50ca19b1d38a0428936989122b4c28bc0d5
heysep <sdvakili@gmail.com>
2015-12-01 14:06:02 -0500

    Add routes for profile get and post

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
 
   post    'signup',  to: 'users#create'
   post    'signin',  to: 'sessions#create'
-  
+
+  get     'profile',  to: 'profiles#show'  
+  post    'profile',  to: 'profiles#create'
+
 end
