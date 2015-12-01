fbd1acb2943a05ba8bd0b2ae793b0755e8e4a250
heysep <sdvakili@gmail.com>
2015-12-01 14:05:47 -0500

    Change the path

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,2 @@
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-## the old way: /posts/:id_partition/:style/:filename
-Paperclip::Attachment.default_options[:path] = '/posts/:model/:user/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:path] = '/:class/:style/:id_partition/:filename'
