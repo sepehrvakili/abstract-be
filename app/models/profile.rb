ea0c122d763d520bdfeb7fd3f57590fe622c301d
heysep <sdvakili@gmail.com>
2015-12-01 14:04:13 -0500

    Add profile model, view, controller

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,9 @@
 class Profile < ActiveRecord::Base
+
+	belongs_to :user
+
+	validates_presence_of :picture, :bio, :website, :location
+	has_attached_file :picture, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>" }
+	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+
 end
