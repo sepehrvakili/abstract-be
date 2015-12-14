0d47ea930379eb59c66f4b92ec8a8422a2021b6c
heysep <sdvakili@gmail.com>
2015-12-14 16:25:29 -0500

    Add post_type conditional validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,8 +14,16 @@ class Post < ActiveRecord::Base
 
 	has_attached_file :image, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>" }
 
-	validates_presence_of :post_type
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+	validates_presence_of :post_type
+	validates :post_type, inclusion: { in: %w(image text link quote moodboard),
+    message: "%{value} is not a valid post type." }, allow_nil: false
+
+	validates_presence_of :image, :title, :description, :if => :image_post?
+	validates_presence_of :status, :if => :text_post?	
+	validates_presence_of :url, :description, :if => :link_post?
+	validates_presence_of :quote, :if => :quote_post?
 
   def tag_phrases
     self.tags.map(&:phrase).join(", ")
@@ -24,6 +32,22 @@ class Post < ActiveRecord::Base
   def tag_phrases=(inputted_phrases)
     tag_models = inputted_phrases.split(", ").map { |phrase| Tag.find_or_create_by(phrase: phrase) }
     self.tags = tag_models
+  end
+
+  def image_post?
+  	self.post_type == "image"
+  end
+
+  def text_post?
+  	self.post_type == "text"
+  end
+
+  def link_post?
+  	self.post_type == "link"
+  end
+
+  def quote_post?
+  	self.post_type == "quote"
   end
 
 end
